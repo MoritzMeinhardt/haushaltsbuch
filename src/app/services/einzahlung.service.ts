ec837717cd2a99004eceb22448e674bff8034f17
Meinhardt, Moritz <Moritz.Meinhardt@EXXETA.com>
2020-11-12 14:53:16 +0100

    add context for this

--- src/app/services/einzahlung.service.ts
+++ src/app/services/einzahlung.service.ts
@@ -1,6 +1,6 @@
-import {Injectable} from '@angular/core';
-import {DataService} from './data.service';
-import {Buchung} from '../models/buchung';
+import { Injectable } from '@angular/core';
+import { DataService } from './data.service';
+import { Buchung } from '../models/buchung';
 
 @Injectable({
     providedIn: 'root'
@@ -12,10 +12,10 @@ export class EinzahlungService {
     ) {
     }
 
-    einzahlen(betrag: number) {
+    einzahlen = ((betrag: number) => {
         const einzahlungsBuchung: Buchung = this.getEinzahlungsBuchung(betrag);
         this.dataService.addBuchung(einzahlungsBuchung);
-    }
+    }).bind(this);
 
     getEinzahlungsBuchung(betrag: number): Buchung {
         return new Buchung(betrag, null);
